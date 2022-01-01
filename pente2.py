16649884434b8c932a36a41f3db1a084016e28c8
Caden Claussen <caden.claussen@gmail.com>
2021-12-31 16:57:00 -0800

    First Test Attempt

--- pente2.py
+++ pente2.py
@@ -2,16 +2,14 @@ import random
 
 def main():
 	board = initializeBoard()
-	currentTurnTokenColor = random.choice(["R", "B"])
+	token = random.choice(["R", "B"])
 	printBoard(board)
-	board = generateRandomBoard(board, currentTurnTokenColor, 150)
+	board = generateRandomBoard(board, token, 150)
 	for i in range(5):
 		print(" ")
 	printBoard(board)
-	checkForOpenThrees(board, currentTurnTokenColor)
-	switchTurns(currentTurnTokenColor)
-	checkForOpenThrees(board, currentTurnTokenColor)
-	switchTurns(currentTurnTokenColor)
+	checkSituations(board, token)
+
 
 def initializeBoard():
 	board = []
@@ -28,23 +26,60 @@ def printBoard(board):
 			print(board[row][col] + "   ", end="")
 		print(" ")
 
-def switchTurns(currentTurnTokenColor):
-	if currentTurnTokenColor == "R":
+def switchTurns(token):
+	if token == "R":
 		return "B"
 	else:
 		return "R"
 
-def generateRandomBoard(board, currentTurnTokenColor, numberOfTurns):
+def generateRandomBoard(board, token, numberOfTurns):
 	for _ in range(numberOfTurns):
 		validSpot = False
 		while validSpot == False:
 			xcord = random.randint(0, 18)
 			ycord = random.randint(0, 18)
 			validSpot = isValidSpot(xcord, ycord, board)
-		board[ycord][xcord] = currentTurnTokenColor
-		currentTurnTokenColor = switchTurns(currentTurnTokenColor)
+		board[ycord][xcord] = token
+		token = switchTurns(token)
 	return board
 
+def checkSituations(board, token):
+	patterns = []
+	for row in range(0, 18):
+		for col in range(0, 18):
+			verify(board, row, col, [1, 0], token, ["open", "token", "token", "token", "open"], "open three", patterns)
+			# verify(board, row, col, [1, 0], token, ["closed", "token", "token", "token", "token", "open"], "closed four", patterns)
+
+def verify(board, row, col, deltas, token, pattern, typeOfPattern, patterns):
+	for patternIndex in range(len(pattern)):
+		isBeadAtSpot = isBead(board, row, col, token, pattern[patternIndex - 1])
+
+def isBead(board, row, col, token, tokenType):
+
+	if row > 18:
+		return False
+	if row < 0:
+		return False
+	if col > 18:
+		return False
+	if col < 0:
+		return False
+
+	if board[row][col] == token and "token" == tokenType:
+		return True
+
+	if token == "R":
+		oposingToken = "B"
+	else:
+		oposingToken = "R"
+
+	if board[row][col] == oposingToken and "closed" == tokenType:
+		return True
+
+	if board[row][col] == "." and "open" == tokenType:
+		return True
+
+	print("Error")
 
 
 main()
