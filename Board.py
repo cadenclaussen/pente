71c006a9e63e7a9a9c4a7c9b1f8461e8d766b115
Shane Claussen <sclaussen@yahoo.com>
2022-01-16 10:08:44 -0800

    Modified controller/model: x y vs row/col & board 2dim storing objects vs string

--- Board.py
+++ Board.py
@@ -2,14 +2,14 @@ import sys
 
 
 class Board:
-    East = { "name": "east", "rowOffset": 0, "columnOffset": 1 }
-    Southeast = { "name": "southeast", "rowOffset": 1, "columnOffset": 1 }
-    South = { "name": "south", "rowOffset": 1, "columnOffset": 0 }
-    Southwest = { "name": "southwest", "rowOffset": 1, "columnOffset": -1 }
-    West = { "name": "west", "rowOffset": 0, "columnOffset": -1 }
-    Northwest = { "name": "northwest", "rowOffset": -1, "columnOffset": -1 }
-    North = { "name": "north", "rowOffset": -1, "columnOffset": 0 }
-    Northeast = { "name": "northeast", "rowOffset": -1, "columnOffset": 1 }
+    East = { 'name': 'east', 'yOffset': 0, 'xOffset': 1 }
+    Southeast = { 'name': 'southeast', 'yOffset': 1, 'xOffset': 1 }
+    South = { 'name': 'south', 'yOffset': 1, 'xOffset': 0 }
+    Southwest = { 'name': 'southwest', 'yOffset': 1, 'xOffset': -1 }
+    West = { 'name': 'west', 'yOffset': 0, 'xOffset': -1 }
+    Northwest = { 'name': 'northwest', 'yOffset': -1, 'xOffset': -1 }
+    North = { 'name': 'north', 'yOffset': -1, 'xOffset': 0 }
+    Northeast = { 'name': 'northeast', 'yOffset': -1, 'xOffset': 1 }
 
 
     def __init__(self):
@@ -17,38 +17,49 @@ class Board:
         self.winningPatterns = []
         self.announcePatterns = { 'Shane': [], 'Caden': [] }
         self.pointPatterns = { 'Shane': [], 'Caden': [] }
+
         self.board = []
-        for row in range(19):
+        for y in range(19):
             self.board.append([])
-            for column in range(19):
-                self.board[row].append(".")
+            for x in range(19):
+                self.board[y].append({ 'bead': 'Open', 'highlight': False, 'weight': 0 })
 
 
-    def removeFromBoard(self, position):
-        self.board[position["column"]][position["row"]] = "."
+    def playBead(self, x, y, color):
+        self.board[y][x]['bead'] = color
 
 
-    def playBead(self, currentPlayer, position):
-        self.board[position["column"]][position["row"]] = currentPlayer.color[0]
+    def removeBead(self, x, y):
+        self.board[y][x]['bead'] = 'Open'
+
+
+    # TODO: Use a consistent position or y/x model
+    def getBead(self, x, y):
+        return self.board[y][x]['bead']
+
+
+    # TODO: Use a consistent position or y/x model
+    def isOpen(self, x, y):
+        return (self.board[y][x]['bead'] == 'Open')
 
 
     # TODO: If there are three players this needs to be updated so the two opponent beads are the same bead
-    def findJumpPatterns(self, currentPlayer, position):
+    def findJumpPatterns(self, currentPlayer, x, y):
         self.jumpPatterns = []
 
         patterns = []
-        patterns.append({ "name": "Jump", "tokens": [ "bead", "opponent", "opponent", "bead" ]})
+        patterns.append({ 'name': 'Jump', 'tokens': [ 'bead', 'opponent', 'opponent', 'bead' ]})
 
         cumulativePatternsFound = []
         for pattern in patterns:
-            patternsFound = self.__findPatternAtPosition(currentPlayer, patterns[0], position, [ Board.East, Board.Southeast, Board.South, Board.Southwest, Board.West, Board.Northwest, Board.North, Board.Northeast ], "opponent")
+            patternsFound = self.__findPatternAtPosition(currentPlayer, patterns[0], x, y, [ Board.East, Board.Southeast, Board.South, Board.Southwest, Board.West, Board.Northwest, Board.North, Board.Northeast ], 'opponent')
             if patternsFound:
                 cumulativePatternsFound += patternsFound
 
         self.jumpPatterns = cumulativePatternsFound
         for jumpPattern in self.jumpPatterns:
-            for position in jumpPattern["positions"]:
-                self.removeFromBoard(position)
+            for position in jumpPattern['positions']:
+                self.removeBead(position['x'], position['y'])
 
         return self.jumpPatterns != []
 
@@ -57,15 +68,15 @@ class Board:
         self.winningPatterns = []
 
         patterns = []
-        patterns.append({ "name": "Five", "tokens": [ "not-bead", "bead", "bead", "bead", "bead", "bead", "not-bead" ] })
-        patterns.append({ "name": "Six", "tokens": [ "not-bead", "bead", "bead", "bead", "bead", "bead", "bead", "not-bead" ] })
-        patterns.append({ "name": "Seven", "tokens": [ "not-bead", "bead", "bead", "bead", "bead", "bead", "bead", "bead", "not-bead" ] })
-        patterns.append({ "name": "Eight", "tokens": [ "not-bead", "bead", "bead", "bead", "bead", "bead", "bead", "bead", "bead", "not-bead" ] })
-        patterns.append({ "name": "Nine", "tokens": [ "not-bead", "bead", "bead", "bead", "bead", "bead", "bead", "bead", "bead", "bead", "not-bead" ] })
+        patterns.append({ 'name': 'Five', 'tokens': [ 'not-bead', 'bead', 'bead', 'bead', 'bead', 'bead', 'not-bead' ] })
+        patterns.append({ 'name': 'Six', 'tokens': [ 'not-bead', 'bead', 'bead', 'bead', 'bead', 'bead', 'bead', 'not-bead' ] })
+        patterns.append({ 'name': 'Seven', 'tokens': [ 'not-bead', 'bead', 'bead', 'bead', 'bead', 'bead', 'bead', 'bead', 'not-bead' ] })
+        patterns.append({ 'name': 'Eight', 'tokens': [ 'not-bead', 'bead', 'bead', 'bead', 'bead', 'bead', 'bead', 'bead', 'bead', 'not-bead' ] })
+        patterns.append({ 'name': 'Nine', 'tokens': [ 'not-bead', 'bead', 'bead', 'bead', 'bead', 'bead', 'bead', 'bead', 'bead', 'bead', 'not-bead' ] })
 
         cumulativePatternsFound = []
         for pattern in patterns:
-            patternsFound = self.__findPattern(currentPlayer, pattern, "bead")
+            patternsFound = self.__findPattern(currentPlayer, pattern, 'bead')
             if patternsFound:
                 cumulativePatternsFound += patternsFound
 
@@ -77,19 +88,19 @@ class Board:
         self.announcePatterns[currentPlayer.name] = []
 
         patterns = []
-        patterns.append({ "name": "Open Three", "tokens": [ "not-bead", "open", "bead", "bead", "bead", "open", "not-bead" ] })
-        patterns.append({ "name": "Holed Open Four", "tokens": [ "open", "bead", "open", "bead", "bead", "open" ] })
-        patterns.append({ "name": "Holed Open Four", "tokens": [ "open", "bead", "bead", "open", "bead", "open" ] })
-        patterns.append({ "name": "Closed Four", "tokens": [ "open", "bead", "bead", "bead", "bead", "closed" ] })
-        patterns.append({ "name": "Closed Four", "tokens": [ "closed", "bead", "bead", "bead", "bead", "open" ] })
-        patterns.append({ "name": "Open Four", "tokens": [ "open", "bead", "bead", "bead", "bead", "open" ] })
-        patterns.append({ "name": "Holed Five", "tokens": [ "not-bead", "bead", "open", "bead", "bead", "bead", "not-bead" ] })
-        patterns.append({ "name": "Holed Five", "tokens": [ "not-bead", "bead", "bead", "open", "bead", "bead", "not-bead" ] })
-        patterns.append({ "name": "Holed Five", "tokens": [ "not-bead", "bead", "bead", "bead", "open", "bead", "not-bead" ] })
+        patterns.append({ 'name': 'Open Three', 'tokens': [ 'not-bead', 'open', 'bead', 'bead', 'bead', 'open', 'not-bead' ] })
+        patterns.append({ 'name': 'Holed Open Four', 'tokens': [ 'open', 'bead', 'open', 'bead', 'bead', 'open' ] })
+        patterns.append({ 'name': 'Holed Open Four', 'tokens': [ 'open', 'bead', 'bead', 'open', 'bead', 'open' ] })
+        patterns.append({ 'name': 'Closed Four', 'tokens': [ 'open', 'bead', 'bead', 'bead', 'bead', 'closed' ] })
+        patterns.append({ 'name': 'Closed Four', 'tokens': [ 'closed', 'bead', 'bead', 'bead', 'bead', 'open' ] })
+        patterns.append({ 'name': 'Open Four', 'tokens': [ 'open', 'bead', 'bead', 'bead', 'bead', 'open' ] })
+        patterns.append({ 'name': 'Holed Five', 'tokens': [ 'not-bead', 'bead', 'open', 'bead', 'bead', 'bead', 'not-bead' ] })
+        patterns.append({ 'name': 'Holed Five', 'tokens': [ 'not-bead', 'bead', 'bead', 'open', 'bead', 'bead', 'not-bead' ] })
+        patterns.append({ 'name': 'Holed Five', 'tokens': [ 'not-bead', 'bead', 'bead', 'bead', 'open', 'bead', 'not-bead' ] })
 
         cumulativePatternsFound = []
         for pattern in patterns:
-            patternsFound = self.__findPattern(currentPlayer, pattern, "bead")
+            patternsFound = self.__findPattern(currentPlayer, pattern, 'bead')
             if patternsFound:
                 cumulativePatternsFound += patternsFound
 
@@ -97,21 +108,21 @@ class Board:
         return self.announcePatterns[currentPlayer.name] != []
 
 
-    # TODO: Include all the winning pattern variants as well
+    # TODO: Resolve the issue of a win ...
     def findPointPatterns(self, currentPlayer):
         self.pointPatterns[currentPlayer.name] = []
 
         patterns = []
-        patterns.append({ "name": "Four", "tokens": [ "not-bead", "bead", "bead", "bead", "bead", "not-bead" ] })
-        patterns.append({ "name": "Five", "tokens": [ "not-bead", "bead", "bead", "bead", "bead", "bead", "not-bead" ] })
-        patterns.append({ "name": "Six", "tokens": [ "not-bead", "bead", "bead", "bead", "bead", "bead", "bead", "not-bead" ] })
-        patterns.append({ "name": "Seven", "tokens": [ "not-bead", "bead", "bead", "bead", "bead", "bead", "bead", "bead", "not-bead" ] })
-        patterns.append({ "name": "Eight", "tokens": [ "not-bead", "bead", "bead", "bead", "bead", "bead", "bead", "bead", "bead", "not-bead" ] })
-        patterns.append({ "name": "Nine", "tokens": [ "not-bead", "bead", "bead", "bead", "bead", "bead", "bead", "bead", "bead", "bead", "not-bead" ] })
+        patterns.append({ 'name': 'Four', 'tokens': [ 'not-bead', 'bead', 'bead', 'bead', 'bead', 'not-bead' ] })
+        patterns.append({ 'name': 'Five', 'tokens': [ 'not-bead', 'bead', 'bead', 'bead', 'bead', 'bead', 'not-bead' ] })
+        patterns.append({ 'name': 'Six', 'tokens': [ 'not-bead', 'bead', 'bead', 'bead', 'bead', 'bead', 'bead', 'not-bead' ] })
+        patterns.append({ 'name': 'Seven', 'tokens': [ 'not-bead', 'bead', 'bead', 'bead', 'bead', 'bead', 'bead', 'bead', 'not-bead' ] })
+        patterns.append({ 'name': 'Eight', 'tokens': [ 'not-bead', 'bead', 'bead', 'bead', 'bead', 'bead', 'bead', 'bead', 'bead', 'not-bead' ] })
+        patterns.append({ 'name': 'Nine', 'tokens': [ 'not-bead', 'bead', 'bead', 'bead', 'bead', 'bead', 'bead', 'bead', 'bead', 'bead', 'not-bead' ] })
 
         cumulativePatternsFound = []
         for pattern in patterns:
-            patternsFound = self.__findPattern(currentPlayer, pattern, "bead")
+            patternsFound = self.__findPattern(currentPlayer, pattern, 'bead')
             if patternsFound:
                 cumulativePatternsFound += patternsFound
 
@@ -122,9 +133,9 @@ class Board:
 
     def __findPattern(self, currentPlayer, pattern, tokenNameToSavePositionFor):
         cumulativePatternsFound = None
-        for row in range(-1, 20):
-            for column in range(-1, 20):
-                patternsFound = self.__findPatternAtPosition(currentPlayer, pattern, { "row": row, "column": column }, [ Board.East, Board.Southeast, Board.South, Board.Southwest ], tokenNameToSavePositionFor)
+        for x in range(-1, 20):
+            for y in range(-1, 20):
+                patternsFound = self.__findPatternAtPosition(currentPlayer, pattern, x, y, [ Board.East, Board.Southeast, Board.South, Board.Southwest ], tokenNameToSavePositionFor)
                 if patternsFound:
                     if cumulativePatternsFound is None:
                         cumulativePatternsFound = []
@@ -132,14 +143,14 @@ class Board:
         return cumulativePatternsFound
 
 
-    def __findPatternAtPosition(self, currentPlayer, pattern, position, directions, tokenNameToSavePositionFor):
+    def __findPatternAtPosition(self, currentPlayer, pattern, x, y, directions, tokenNameToSavePositionFor):
         patternsFound = None
         for direction in directions:
-            positionsFound = self.__findPatternAtPositionInDirection(currentPlayer, pattern, position, direction, tokenNameToSavePositionFor)
+            positionsFound = self.findPatternAtPositionInDirection(currentPlayer, pattern, x, y, direction, tokenNameToSavePositionFor)
             if positionsFound:
                 if patternsFound is None:
                     patternsFound = []
-                patternsFound += [ { "name": pattern["name"], "direction": direction["name"], "positions": positionsFound } ]
+                patternsFound += [ { 'name': pattern['name'], 'direction': direction['name'], 'positions': positionsFound } ]
 
         return patternsFound
 
@@ -148,53 +159,60 @@ class Board:
     # - None if the pattern did not match in the direction
     # - An array of matched positions if the pattern was detected
     #   Note: If there are no detected positions but the patterns was found, then [] is returned
-    def __findPatternAtPositionInDirection(self, currentPlayer, pattern, position, direction, tokenNameToSavePositionFor):
+    def findPatternAtPositionInDirection(self, currentPlayer, pattern, x, y, direction, tokenNameToSavePositionFor):
         positionsFound = []
-        for token in pattern["tokens"]:
-            if not self.expectedTokenAtPosition(currentPlayer, position, token):
+        for token in pattern['tokens']:
+            if not self.__expectedTokenAtPosition(currentPlayer, x, y, token):
+                # print('RETURNING')
+                # print()
                 return None
 
             if token == tokenNameToSavePositionFor:
-                positionsFound.append({ "row": position["row"], "column": position["column"] })
+                positionsFound.append({ 'x': x, 'y': y })
 
             # Update the position in the appropriate direction to get ready to look for the next token in the pattern
-            position = { "row": (position["row"] + direction["rowOffset"]), "column": (position["column"] + direction["columnOffset"])}
+            x += direction['xOffset']
+            y += direction['yOffset']
 
-        # If we made it this far, all the tokens in the pattern were
+        # If we made it this far, all the tokens in the pattern were found
+        # print('Found pattern at position in direction')
+        # print()
         return positionsFound
 
 
-    def expectedTokenAtPosition(self, currentPlayer, position, token):
-        row = position["row"]
-        column = position["column"]
+    def __expectedTokenAtPosition(self, currentPlayer, x, y, token):
 
         # bead
         #
         # matches a bead played at the position by the current player
-        if token == "bead":
-            if row > 18 or row < 0 or column > 18 or column < 0:
+        if token == 'bead':
+            # print('Looking for bead ' + str(x) + ' ' + str(y))
+            if x > 18 or x < 0 or y > 18 or y < 0:
+                # print('  NOT found (out of bounds)')
                 return False
-            if self.board[column][row] == currentPlayer.color[0]:
+            if self.getBead(x, y) == currentPlayer.color:
+                # print('  Found')
                 return True
+            # print('  NOT found (was not the players bead)')
             return False
 
         # opponent
         #
         # matches a bead played at the position by an opposing player
-        if token == "opponent":
-            if row > 18 or row < 0 or column > 18 or column < 0:
+        if token == 'opponent':
+            if x > 18 or x < 0 or y > 18 or y < 0:
                 return False
-            if self.board[column][row] != '.' and self.board[column][row] != currentPlayer.color[0]:
+            if not self.isOpen(x, y) and self.getBead(x, y) != currentPlayer.color:
                 return True
             return False
 
         # open
         #
         # matches a position with no bead
-        if token == "open":
-            if row > 18 or row < 0 or column > 18 or column < 0:
+        if token == 'open':
+            if x > 18 or x < 0 or y > 18 or y < 0:
                 return False
-            if self.board[column][row] == '.':
+            if self.isOpen(x, y):
                 return True
             return False
 
@@ -205,13 +223,21 @@ class Board:
         # 1. a position that is off the board
         # 2. another player's bead
         # 3. an open position
-        if token == "not-bead":
-            if row > 18 or row < 0 or column > 18 or column < 0:
+        if token == 'not-bead':
+            # print('Looking for not-bead ' + str(x) + ' ' + str(y))
+            if x > 18 or x < 0 or y > 18 or y < 0:
+                # print('  Found (out of bounds)')
                 return True
-            if self.board[column][row] != '.' and self.board[column][row] != currentPlayer.color[0]:
+
+            if (self.getBead(x, y) != currentPlayer.color):
+                # print('  Found (not players bead)')
                 return True
-            if self.board[column][row] == ".":
-                return True
+
+            # if (not self.isOpen(x, y)) and self.getBead(x, y) != currentPlayer.color:
+            #     return True
+            # if self.isOpen(x, y):
+            #     return True
+            # print('  NOT Found')
             return False
 
         # closed
@@ -219,10 +245,10 @@ class Board:
         # Matches two scenarios:
         # 1. a position that is off the board
         # 2. a position occupied by an opposing player's bead
-        if token == "closed":
-            if (row > 18 or row < 0 or column > 18 or column < 0):
+        if token == 'closed':
+            if (x > 18 or x < 0 or y > 18 or y < 0):
                 return True
-            if self.board[column][row] != '.' and self.board[column][row] != currentPlayer.color[0]:
+            if not self.isOpen(x, y) and self.getBead(x, y) != currentPlayer.color:
                 return True
             return False
 
@@ -231,13 +257,16 @@ class Board:
 
 
     def __str__(self):
-        s = "   "
-        for column in range(19):
-            s += str(column  % 10) + " "
-        s += "\n"
-        for row in range(19):
-            s += str(row % 10)  + "  "
-            for column in range(19):
-                s += str(self.board[column][row]) + " "
-            s += "\n"
+        s = '   '
+        for x in range(19):
+            s += str(x  % 10) + ' '
+        s += '\n'
+        for y in range(19):
+            s += str(y % 10)  + '  '
+            for x in range(19):
+                if self.isOpen(x, y):
+                    s += '. '
+                else:
+                    s += self.getBead(x, y)[0] + ' '
+            s += '\n'
         return s
--- test.py
+++ test.py
@@ -4,47 +4,59 @@ from Player import Player
 from Game import Game
 
 
+def testBoard():
+    print()
+    print('testBoard')
+    print()
+    game, board, players, currentPlayer = __init()
+    print(board)
+
+
 def testWinningPatterns():
     print()
-    print("testWinningPatterns")
+    print('testWinningPatterns')
+    print()
     game, board, players, currentPlayer = __init()
-    __generateBeadsInARow(board, { "row": 0, "column": 0 }, Board.South, 9)
-    __generateBeadsInARow(board, { "row": 0, "column": 18 }, Board.South, 9)
-    __generateBeadsInARow(board, { "row": 2, "column": 2 }, Board.Southeast, 8)
-    __generateBeadsInARow(board, { "row": 9, "column": 9 }, Board.East, 7)
-    __generateBeadsInARow(board, { "row": 9, "column": 9 }, Board.South, 5)
-    __generateBeadsInARow(board, { "row": 4, "column": 4 }, Board.East, 3)
-    __generateBeadsInARow(board, { "row": 6, "column": 6 }, Board.East, 4)
+    __generateBeadSequence(board, 0, 0, Board.South, 9)
+    __generateBeadSequence(board, 18, 0, Board.South, 9)
+    __generateBeadSequence(board, 0, 18, Board.East, 6)
+    __generateBeadSequence(board, 2, 2, Board.Southeast, 8)
+    __generateBeadSequence(board, 9, 9, Board.East, 7)
+    __generateBeadSequence(board, 9, 9, Board.South, 5)
+    __generateBeadSequence(board, 4, 4, Board.East, 3)
+    __generateBeadSequence(board, 6, 6, Board.East, 4)
     __printWinningPatterns(board, players, currentPlayer)
 
 
 def testJumpPatterns():
     print()
-    print("testJumpPatterns")
+    print('testJumpPatterns')
+    print()
     game, board, players, currentPlayer = __init()
-    __generateJumpPattern(board, { "row": 9, "column": 9 }, Board.North)
-    __generateJumpPattern(board, { "row": 9, "column": 9 }, Board.Northeast)
-    __generateJumpPattern(board, { "row": 9, "column": 9 }, Board.East)
-    __generateJumpPattern(board, { "row": 9, "column": 9 }, Board.Southeast)
-    __generateJumpPattern(board, { "row": 9, "column": 9 }, Board.South)
-    __generateJumpPattern(board, { "row": 9, "column": 9 }, Board.Southwest)
-    __generateJumpPattern(board, { "row": 9, "column": 9 }, Board.West)
-    __generateJumpPattern(board, { "row": 9, "column": 9 }, Board.Northwest)
+    __generateJumpPattern(board, 9, 9, Board.North)
+    __generateJumpPattern(board, 9, 9, Board.Northeast)
+    __generateJumpPattern(board, 9, 9, Board.East)
+    __generateJumpPattern(board, 9, 9, Board.Southeast)
+    __generateJumpPattern(board, 9, 9, Board.South)
+    __generateJumpPattern(board, 9, 9, Board.Southwest)
+    __generateJumpPattern(board, 9, 9, Board.West)
+    __generateJumpPattern(board, 9, 9, Board.Northwest)
     __printJumpPatterns(board, players, currentPlayer)
 
 
 def testAnnouncePatterns():
     print()
-    print("testAnnouncePatterns")
+    print('testAnnouncePatterns')
+    print()
     game, board, players, currentPlayer = __init()
-    __generateRandomBoard(board, players, 150)
     __printWinningPatterns(board, players, currentPlayer)
     __printAnnouncePatterns(board, players, currentPlayer)
 
 
 def testPointPatterns():
     print()
-    print("testPointPatterns")
+    print('testPointPatterns')
+    print()
     game, board, players, currentPlayer = __init()
     __generateRandomBoard(board, players, 150)
     __printPointPatterns(board, players, currentPlayer)
@@ -54,89 +66,91 @@ def __printWinningPatterns(board, players, currentPlayer):
     print()
     print(board)
     print()
-    print("Winning Patterns")
+    print('Winning Patterns')
     board.findWinningPatterns(currentPlayer)
     for winningPattern in board.winningPatterns:
-        print(winningPattern["name"] + __positions(winningPattern["positions"]))
+        print(winningPattern['name'] + __positions(winningPattern['positions']))
 
 
 def __printJumpPatterns(board, players, currentPlayer):
     print()
-    board.findJumpPatterns(currentPlayer, { "row": 9, "column": 9 })
+    board.findJumpPatterns(currentPlayer, x, y)
+    print()
     print(board)
     print()
-    print("Jump Patterns")
+    print('Jump Patterns')
     for jumpPattern in board.jumpPatterns:
-        print(__positions(jumpPattern["positions"]))
+        print(__positions(jumpPattern['positions']))
 
 
 def __printAnnouncePatterns(board, players, currentPlayer):
     print()
     print(board)
     print()
-    print("Announce Patterns")
+    print('Announce Patterns')
     board.findAnnouncePatterns(players[0])
     for announcePattern in board.announcePatterns[players[0].name]:
-        print(players[0].name + ": " + announcePattern["name"] + __positions(announcePattern["positions"]))
+        print(players[0].name + ': ' + announcePattern['name'] + __positions(announcePattern['positions']))
     board.findAnnouncePatterns(players[1])
     for announcePattern in board.announcePatterns[players[1].name]:
-        print(players[1].name + ": " + announcePattern["name"] + __positions(announcePattern["positions"]))
+        print(players[1].name + ': ' + announcePattern['name'] + __positions(announcePattern['positions']))
 
 
 def __printPointPatterns(board, players, currentPlayer):
     print()
     print(board)
     print()
-    print("Point Patterns")
+    print('Point Patterns')
     board.findPointPatterns(players[0])
     for pointPattern in board.pointPatterns[players[0].name]:
-        print(players[0].name + ": " + pointPattern["name"] + __positions(pointPattern["positions"]))
+        print(players[0].name + ': ' + pointPattern['name'] + __positions(pointPattern['positions']))
     board.findPointPatterns(players[1])
     for pointPattern in board.pointPatterns[players[1].name]:
-        print(players[1].name + ": " + pointPattern["name"] + __positions(pointPattern["positions"]))
+        print(players[1].name + ': ' + pointPattern['name'] + __positions(pointPattern['positions']))
 
 
 def __positions(positions):
-    s = ""
+    s = ''
     for position in positions:
-        s += " (" + str(position["column"]) + "," + str(position["row"]) + ")"
+        s += ' (' + str(position['x']) + ',' + str(position['y']) + ')'
     return s
 
 
 def __init():
-    player1 = Player("Shane", "Blue", 0)
-    player2 = Player("Caden", "Red", 1)
+    player1 = Player('Shane', 'Blue', 0)
+    player2 = Player('Caden', 'Red', 1)
     return Game(), Board(), [ player1, player2], player1
 
 
-def __generateBeadsInARow(board, position, direction, count):
+def __generateBeadSequence(board, x, y, direction, count):
     for i in range(count):
-        board.board[position["column"] + (direction["columnOffset"] * i)][position["row"] + (direction["rowOffset"] * i)] = "B"
+        board.playBead(x + direction['xOffset'] * i, y + direction['yOffset'] * i, 'Blue')
 
 
-def __generateJumpPattern(board, position, direction):
-    board.board[position["column"]][position["row"]] = 'B'
-    board.board[position["column"] + direction["columnOffset"]][position["row"] + direction["rowOffset"]] = 'R'
-    board.board[position["column"] + (2 * direction["columnOffset"])][position["row"] + (2 * direction["rowOffset"])] = 'R'
-    board.board[position["column"] + (3 * direction["columnOffset"])][position["row"] + (3 * direction["rowOffset"])] = 'B'
+def __generateJumpPattern(board, x, y, direction):
+    board.playBead(x, y, 'Blue')
+    board.playBead(x + direction['xOffset'], y + direction['yOffset'], 'Red')
+    board.playBead(x + (2 * direction['xOffset']), y + (2 * direction['yOffset']), 'Red')
+    board.playBead(x + (3 * direction['xOffset']), y + (3 * direction['yOffset']), 'Blue')
 
 
 def __generateRandomBoard(board, players, numberOfTurns):
     currentPlayer = players[0]
     for _ in range(numberOfTurns):
         while True:
-            row = random.randint(0, 18)
-            column = random.randint(0, 18)
-            if board.board[column][row] == '.':
+            x = random.randint(0, 18)
+            y = random.randint(0, 18)
+            if board.getBead(x, y) == 'Open':
                 break
-        board.board[column][row] = currentPlayer.color[0]
+        board.playBead(x, y, currentPlayer.color)
         if currentPlayer.key == 0:
             currentPlayer = players[1]
         else:
             currentPlayer = players[0]
 
 
+# testBoard()
 testWinningPatterns()
-testJumpPatterns()
-testAnnouncePatterns()
-testPointPatterns()
+# testJumpPatterns()
+# testAnnouncePatterns()
+# testPointPatterns()
